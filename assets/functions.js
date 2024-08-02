789b3d8fbe524aeea0a1c6174aaa95afed439850
Mauricio Brito dos Santos <164758301+MauAnalyst@users.noreply.github.com>
2024-08-02 17:42:56 -0300

    Update functions.js

--- assets/functions.js
+++ assets/functions.js
@@ -22,21 +22,24 @@ function UpInputNumber(valorInput){
         if (!event.data.match(regex)){
             return valorInput.value = '0,00';
             //console.log('caiu aq?')
-        };
+        }
 
         let valorDigitado = valorInput.value.trim().replace(/[^0-9]/g, "");
+
 
         if (valorDigitado.startsWith("000", 0) || valorDigitado.length == 1) {
             digitoAnterior = event.data;
             return valorInput.value = "0,0" + event.data;
 
         } else if (valorDigitado.startsWith("00", 0)) {
+
             return valorInput.value = "0," + digitoAnterior + event.data; 
+            
             
         } else {
             if (valorDigitado.length > 2) {
                 valorDigitado = valorDigitado.replace(/^0+/, '');
-            };
+            }
             
             let parteInteira = valorDigitado.slice(0, -2) || "0";
             let parteDecimal = valorDigitado.slice(-2);
@@ -45,33 +48,15 @@ function UpInputNumber(valorInput){
 
             if (parteInteira.length < 2) {
                 parteInteira = "" + parteInteira;
-            };
+            }
             
             valorInput.value = parteInteira + "," + parteDecimal;
-        };
+        }
 
         
     });
 
     //deletando input
-    valorInput.addEventListener('keydown', function(e){
-        //sconsole.log('asd')
-        if (e.key === 'Delete') {
-            valorInput.value = '0,00'
-        } else if (e.key === 'Backspace') {
-            let valorDigitado = valorInput.value.trim().replace(/[^0-9]/g, "");
-            let ValorDigitadoAjuste = +valorDigitado;
-            //console.log(ValorDigitadoAjuste.toString().slice(0,-1));
-
-            if (valorDigitado.length === 3){
-                valorInput.value = "0,0" + ValorDigitadoAjuste.toString().slice(0,-1);
-            } else if(valorDigitado.length === 2){
-                valorInput.value = "0," + ValorDigitadoAjuste.toString();
-            };
-
-        };
-    });
-
     valorInput.addEventListener('keydown', function(e) {
         if (e.key === 'Backspace') {
             e.preventDefault(); // Evita o comportamento padrão do Backspace
